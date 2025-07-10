30d8bc27a17f4809aad826997d5e2119d185006d
DevTreeStudios <122764654+DevTreeStudios@users.noreply.github.com>
2025-07-09 20:50:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const http = require('http');
 const app = express();
 
+app.use(express.json());
+
 const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
